ddf0a37524c65ac596fe0c2947a0ebaab96cdc00
Joseph Ravenwolfe <joseph@ravenwolfe.io>
2023-03-02 11:24:51 -0500

    Add README

---
+++ README.md
@@ -0,0 +1,10 @@
+gs
+
+# How to Run
+
+- Git Clone: `git clone git@github.com:joseph-ravenwolfe/temporal-activity.git`
+- Run `brew install direnv`
+- Run `echo 'eval "$(direnv hook zsh)"' >> ~/.zshrc`
+- Reboot terminal
+- `npm install`
+- Hold for further instruction
--- worker.ts
+++ worker.ts
@@ -2,12 +2,10 @@ import Temporal from './temporal'
 import * as activities from './activities';
 
 (async function() {
-
   const worker = await Temporal.createWorker({
     workflowsPath: require.resolve('./workflows'),
     activities,
     taskQueue: 'hello-world'
   });
   await worker.run();
-
 }());
